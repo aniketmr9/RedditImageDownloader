6aa9c2bee113390aea91ec1c082f18326b499399
Aniket <aniketmr9@gmail.com>
2019-06-29 21:18:43 +0530

    added comments

--- venv/downloadredditimage.py
+++ venv/downloadredditimage.py
@@ -1,13 +1,13 @@
 import praw,requests,re
 
-r = praw.Reddit(client_id='client_id',
-                     client_secret='client_secret',
-                     username='username',
-                     password='password',
-                     user_agent='testbot by /u/aniketmr10')
+r = praw.Reddit(client_id='client_id', #add your client id
+                     client_secret='client_secret', #add your client secret
+                     username='username', #reddit username
+                     password='password', #reddit password
+                     user_agent='testbot by /u/aniketmr10') #message, can be anything
 #r = praw.Reddit("Get top post")
 urls = []
-subreddit = r.subreddit('EarthPorn')
+subreddit = r.subreddit('EarthPorn') #mention the name of the subreddit eg. r/EarthPorn, r/games
 posts = subreddit.top(limit=10)
 for post in posts:
     url = (post.url)
